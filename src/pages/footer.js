1ecd00a0a48a0eaad7ae9bfd30e6b7d93619932a
Hiroshi Mori <hmori@nig.ac.jp>
2023-02-24 14:08:12 +0900

    Update footer.js

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -8,6 +8,35 @@ function Footer() {
     return (
       <footer className="footer">
         <div className="container">
+          <p className="text-muted">© MicrobeDB.JP project team 2019 | Except where otherwise noted, content on this site is licensed under a Creative Commons Attribution 4.0 International license (CC-BY-4.0)</p>
+          <a href=""> <img src="/images/logo_nbdc.png" alt="nbdclogo" width="162.125" height="47.5" /></a>
+        </div>  
+      </footer>
+    )
+  } else if (PROJECT_TYPE=="a") {
+    return (
+      <footer className="footer">
+        <div className="container">
+          <p className="text-muted">© MicrobeDB.JP project team 2019 | Except where otherwise noted, content on this site is licensed under a Creative Commons Attribution 4.0 International license (CC-BY-4.0)</p>
+          <a href=""> <img src="/images/logo_nbdc.png" alt="nbdclogo" width="162.125" height="47.5" /></a>
+        </div>  
+      </footer>
+    )
+  } else if(PROJECT_TYPE=="b") {
+    return (
+      <footer className="footer">
+        <div className="container">
+          <p className="text-muted">© MicrobeDB.JP project team 2019 | Except where otherwise noted, content on this site is licensed under a Creative Commons Attribution 4.0 International license (CC-BY-4.0)</p>
+          <a href=""> <img src="/images/logo_nbdc.png" alt="nbdclogo" width="162.125" height="47.5" /></a>
+        </div>  
+      </footer>
+    )
+  } else if(PROJECT_TYPE=="c") {
+    return (
+      <footer className="footer">
+        <div className="container">
+          <p className="text-muted">© MicrobeDB.JP project team 2019 | Except where otherwise noted, content on this site is licensed under a Creative Commons Attribution 4.0 International license (CC-BY-4.0)</p>
+          <a href=""> <img src="/images/logo_nbdc.png" alt="nbdclogo" width="162.125" height="47.5" /></a>
         </div>  
       </footer>
     )
