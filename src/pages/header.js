cd07a3ad72efb33894dbda442343a5ee1f9456b0
Hiroshi Mori <hmori@nig.ac.jp>
2023-02-24 14:17:20 +0900

    Update header.js

--- src/pages/header.js
+++ src/pages/header.js
@@ -22,7 +22,88 @@ function Header() {
       </div>
     </Navbar>
     ) 
-  } 
+  }
+ else if(PROJECT_TYPE == "a") {
+    return (
+      <Navbar id="myNavbar"  className="navbar navbar-default navbar-fixed-top" style={{ marginBottom: 0, witdh: '100%' }} role="navigation">
+      <div className="container-fluid">
+  
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="nav navbar-nav">
+            <li><Link to="/"><strong>Project A Home</strong></Link></li>
+            <li><Link to="/"><strong>Document</strong></Link></li>
+            <li className="dropdown"> <a href="#" className="dropdown-toggle" data-toggle="dropdown"><strong>Analysis</strong></a>
+              <ul className="dropdown-menu">
+                <li className="dropdown-header">Comparative analysis: </li>
+                <li><Link to="/analysis/metagenome">Metagenomic samples</Link></li>
+              </ul>
+            </li>
+          </ul>
+          <form className="navbar-form navbar-left" action="/search" role="search">
+            <div className="form-group">
+              <input type="text" className="form-control" name="q1" placeholder="e.g. hot spring, Enterococcus faecalis, psbA" size="40" />
+            </div>
+            <button type="submit" className="btn btn-default">Search</button>
+          </form>
+  
+        </div>
+      </div>
+    </Navbar>
+    ) 
+  } else if(PROJECT_TYPE == "b") {
+    return (
+      <Navbar id="myNavbar"  className="navbar navbar-default navbar-fixed-top" style={{ marginBottom: 0, witdh: '100%' }} role="navigation">
+      <div className="container-fluid">
+  
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="nav navbar-nav">
+            <li><Link to="/"><strong>Project B Home</strong></Link></li>
+            <li><Link to="/"><strong>Document</strong></Link></li>
+            <li className="dropdown"> <a href="#" className="dropdown-toggle" data-toggle="dropdown"><strong>Analysis</strong></a>
+              <ul className="dropdown-menu">
+                <li className="dropdown-header">Comparative analysis: </li>
+                <li><Link to="/analysis/metagenome">Metagenomic samples</Link></li>
+              </ul>
+            </li>
+          </ul>
+          <form className="navbar-form navbar-left" action="/search" role="search">
+            <div className="form-group">
+              <input type="text" className="form-control" name="q1" placeholder="e.g. hot spring, Enterococcus faecalis, psbA" size="40" />
+            </div>
+            <button type="submit" className="btn btn-default">Search</button>
+          </form>
+  
+        </div>
+      </div>
+    </Navbar>
+    ) 
+  } else if(PROJECT_TYPE == "c") {
+    return (
+      <Navbar id="myNavbar"  className="navbar navbar-default navbar-fixed-top" style={{ marginBottom: 0, witdh: '100%' }} role="navigation">
+      <div className="container-fluid">
+  
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="nav navbar-nav">
+            <li><Link to="/"><strong>Project C Home</strong></Link></li>
+            <li><Link to="/"><strong>Document</strong></Link></li>
+            <li className="dropdown"> <a href="#" className="dropdown-toggle" data-toggle="dropdown"><strong>Analysis</strong></a>
+              <ul className="dropdown-menu">
+                <li className="dropdown-header">Comparative analysis: </li>
+                <li><Link to="/analysis/metagenome">Metagenomic samples</Link></li>
+              </ul>
+            </li>
+          </ul>
+          <form className="navbar-form navbar-left" action="/search" role="search">
+            <div className="form-group">
+              <input type="text" className="form-control" name="q1" placeholder="e.g. hot spring, Enterococcus faecalis, psbA" size="40" />
+            </div>
+            <button type="submit" className="btn btn-default">Search</button>
+          </form>
+  
+        </div>
+      </div>
+    </Navbar>
+    ) 
+  }
 }
-
 export default Header
